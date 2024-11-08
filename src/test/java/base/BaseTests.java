d5b9780eab5d3424fa0b2e1e7b8ff75f14a66da0
Daniel Farias <dj.luis198@gmail.com>
2024-11-08 15:29:36 -0300

    primer commit

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import pages.HomePage;
+
+public class BaseTests {
+
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeTest
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        homePage = new HomePage(driver);
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
